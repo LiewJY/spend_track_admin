110c7f9b4d6eaa36fab3d525216f066f8fb4bb83
Liew Jun Young <liewjunyoung@gmail.com>
2023-07-19 23:38:10 +0800

    add setting title

--- lib/setting/view/setting_screen_content.dart
+++ lib/setting/view/setting_screen_content.dart
@@ -5,34 +5,29 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:track_admin/app/app.dart';
 import 'package:track_admin/l10n/l10n.dart';
+import 'package:track_admin/widgets/widgets.dart';
 import 'package:track_theme/track_theme.dart';
 
 class SettingScreenContent extends StatelessWidget {
   const SettingScreenContent({super.key});
-
-  
 
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
     return Column(
       children: [
-        Text(l10n.setting),
+        PageTitleText(title: l10n.setting),
         FilledButton(
           //style: AppStyle.fullWidthButton,
           onPressed: () => context.read<AppBloc>().add(AppLogoutRequested()),
           child: Text(l10n.logout),
         ),
-        FilledButton(
-          //style: AppStyle.fullWidthButton,
-          onPressed: () => log('dddd'),
-          child: Text(l10n.logout),
-        ),
+        // FilledButton(
+        //   //style: AppStyle.fullWidthButton,
+        //   onPressed: () => log('dddd'),
+        //   child: Text(l10n.logout),
+        // ),
       ],
     );
   }
-
-
 }
-
-
