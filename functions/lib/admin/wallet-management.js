5017b8fb851c9532acccae1cfd02605efdd7607c
Liew Jun Young <liewjunyoung@gmail.com>
2023-07-19 22:47:15 +0800

    complete update across

--- functions/lib/admin/wallet-management.js
+++ functions/lib/admin/wallet-management.js
@@ -9,4 +9,21 @@ const { getAuth } = require("firebase-admin/auth");
 // firebase function logging
 require("firebase-functions/logger/compat");
 
-//todo updatewallet --> admin (wallets) -> user (mywallets)
+exports.updateWallet = functions.firestore.document('wallets/{walletId}').onUpdate(async (change, context) => {
+  const after = change.after.data();
+  const walletId = context.params.walletId;
+
+  const usersCollection = admin.firestore().collection('users');
+  const usersCollectionSnapshot = await usersCollection.get();
+  usersCollectionSnapshot.forEach(async doc => {
+    //create batch write
+    const batch = admin.firestore().batch();
+    const walletRef = usersCollection.doc(doc.id).collection('myWallets').where('uid', '==', walletId);
+    const walletSnapshot = await walletRef.get();
+    walletSnapshot.forEach(doc => {
+      batch.update(doc.ref, { 'description': after.description, 'name': after.name });
+    });
+    //batch write to firestore
+    await batch.commit();
+  });
+});
